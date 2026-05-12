d677ac382066c9b84ef1e3351d7f5c2dd67b3a47
Sean O'Leary <seamicole@gmail.com>
2026-05-12 13:45:55 +0700

    Configure TS autocomplete

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -1,8 +1,14 @@
 return {
   "neovim/nvim-lspconfig",
   lazy = false,
+  dependencies = {
+    "hrsh7th/cmp-nvim-lsp",
+  },
   config = function()
+    local capabilities = require("cmp_nvim_lsp").default_capabilities()
+
     vim.lsp.config("ts_ls", {
+      capabilities = capabilities,
       cmd = { "typescript-language-server", "--stdio" },
       filetypes = {
         "javascript",
@@ -24,6 +30,7 @@ return {
     vim.lsp.enable("ts_ls")
 
     vim.lsp.config("rust_analyzer", {
+      capabilities = capabilities,
       on_attach = function(client, bufnr)
         if client.supports_method("textDocument/formatting") then
           vim.api.nvim_create_autocmd("BufWritePre", {
